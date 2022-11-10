f9224d19ed65c1b188f7b2a98a2080f4160d00b7
botechia-erika <mendoncabotechia@proton.me>
2022-11-10 09:05:48 -0300

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const myAppHeader = addEventListener.querySelector('#myApp')
+</header>
+
+app.get('*', (req, res)=>{
+   res.sendFile(myApp.appendChild(`<a type="module" href=[id="#navbar"]"></a>)
+})
+
+
+module.exports = app()
